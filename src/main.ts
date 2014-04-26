26a14bf5025ee96661600bb1ebbc5e36524e7a7f
johnfn <johnfn@gmail.com>
2014-04-25 22:07:07 -0700

    Gotta do this crap ahead of time.
    Maps!

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,77 @@
 /// <reference path="../d/phaser.d.ts" />
 
-var cursors:Phaser.CursorKeys;
-
-class Player extends Phaser.Sprite {
-	body:Phaser.Physics.Arcade.Body;
-
-	constructor(game:Phaser.Game) {
-		super(game, 0, 0, "player", 0);
-
-		game.physics.enable(this, Phaser.Physics.ARCADE);
-	}
-
-	update():void {
-		if (cursors.left.isDown) {
-			this.body.velocity.x = -300;
-		} else if (cursors.right.isDown) {
-			this.body.velocity.x = 300;
-		} else {
-			this.body.velocity.x = 0;
-		}
-	}
-}
+var keyboard:Phaser.Keyboard;
 
 class MainState extends Phaser.State {
+	p:Player;
+	b:Phaser.TilemapLayer;
 	public preload():void {
 		this.load.spritesheet("player","assets/player.png",25,25,1,0,0);
+		this.load.spritesheet("block","assets/block.png",25,25,1,0,0);
+		this.load.tilemap("map", "assets/map.json", null, Phaser.Tilemap.TILED_JSON);
 	}
 
 	public init():void {
 	}
 
 	public create():void {
-		cursors = this.game.input.keyboard.createCursorKeys();
+		keyboard = this.game.input.keyboard;
 
-		var p:Player = new Player(this.game);
+		this.p = new Player(this.game);
+		this.game.add.existing(this.p);
 
-		this.game.add.existing(p);
+		var tileset:Phaser.Tilemap = this.game.add.tilemap("map",25,25,30,30); // w,h, mapw, maph
+		tileset.addTilesetImage("main","block",25, 25);
 
-		//this.game.add.sprite(0, 0, "player");
+		tileset.setCollisionBetween(1,151,true,"collision");
+		this.b = tileset.createLayer("collision");
 	}
 
 	public update():void {
-		
+		this.game.physics.arcade.collide(this.p, this.b);
+	}
+}
+
+class Entity extends Phaser.Sprite {
+	body:Phaser.Physics.Arcade.Body;
+
+	constructor(game:Phaser.Game, spritesheet:string) {
+		super(game, 0, 0, spritesheet, 0);
+
+		game.physics.enable(this, Phaser.Physics.ARCADE);
+	}
+}
+
+class Player extends Entity {
+	constructor(game:Phaser.Game) {
+		super(game, "player");
+
+		this.body.gravity = new Phaser.Point(0, 1600);
+	}
+
+
+	update():void {
+		if (keyboard.isDown(Phaser.Keyboard.LEFT)) {
+			this.body.velocity.x = -300;
+		} else if (keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+			this.body.velocity.x = 300;
+		} else {
+			this.body.velocity.x = 0;
+		}
+
+		if (keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+			if (this.body.touching.down) {
+				this.body.velocity.y = -1200;
+			}
+		}
+	}
+}
+
+class Block extends Entity {
+	constructor(game:Phaser.Game) {
+		super(game, "block");
+
+		this.body.immovable = true;
 	}
 }
 
@@ -49,6 +79,7 @@ class Game {
 	game:Phaser.Game;
 	state: Phaser.State;
 
+	// TODO... browsers w/o WEBGL...
 	constructor() {
 		this.state = new MainState();
 		this.game = new Phaser.Game(800, 600, Phaser.WEBGL, "main", this.state);
